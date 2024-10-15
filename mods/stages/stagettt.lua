4c4e3a1b34a9c55bb5e89f8d0e47678c0fa856d4
wtlas <braydenstutzman01@gmail.com>
2024-10-15 01:29:11 -0600

    FNF TTT
    main files (stage, character, song, added)
    
    waiting on story menu background/backgrounds, character icons, menu backgrounds, intro animation (gf bumping replacement)
    
    maybe pause music, death music, menu music, etc.

---
+++ mods/stages/stagettt.lua
@@ -0,0 +1,25 @@
+function onCreate()
+
+	makeLuaSprite('stage','weekttt/stagettt',-400,-100)
+	addLuaSprite('stage',false)
+	
+	makeLuaSprite('table','weekttt/table',-23,620)
+	addLuaSprite('table',false)
+	
+	scaleObject('table', 1, 1.2)
+	
+	close(true)
+
+end
+
+function onBeatHit()
+
+end
+
+function onStepHit()
+
+end
+
+function onUpdate()
+
+end
